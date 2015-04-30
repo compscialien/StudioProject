7316d292382715b2d79b46a8512c69b0e8e32bc5
Jesse Janowiak <wonkeythemonkey@gmail.com>
2015-04-29 22:23:00 -0400

    Created new "Level01" scene; added basic camera movement.

---
+++ StudioProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Amount of time to wait before camera starts moving (optional)
+	public float delay = 0;
+	float startTime;
+
+	// How fast the camera is moving. Default is 1, set new
+	// values in GUI
+	public float cameraSpeed = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time + delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > startTime) {
+			transform.Translate (Vector2.right * cameraSpeed * Time.deltaTime);
+		}
+	}
+}
