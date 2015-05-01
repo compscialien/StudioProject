7304051d4e702f1ce39ce110a65d7398b2e61ad5
Jesse Janowiak <wonkeythemonkey@gmail.com>
2015-04-30 20:52:12 -0400

    When starting at StartScreen, jump button loads TestingLevel. We can change this to Level01 when it is ready.

---
+++ StudioProject/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Jump")){
+			Application.LoadLevel ("TestingScene");
+		}
+	}
+}
