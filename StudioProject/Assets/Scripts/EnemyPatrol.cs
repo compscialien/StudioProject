1f8ace3849df328f0520547f2ccfab2701998afc
Jesse Janowiak <wonkeythemonkey@gmail.com>
2015-05-03 18:02:07 -0400

    Start / Game Over screen now functions at start and end of game; Jumping while dead no longer a problem;

--- StudioProject/Assets/Scripts/EnemyPatrol.cs
+++ StudioProject/Assets/Scripts/EnemyPatrol.cs
@@ -23,8 +23,6 @@ public class EnemyPatrol : MonoBehaviour {
 	 */
 	public bool movingRight = true;
 
-	// Whether or not the enemy object is rotated 180 degrees in Z
-	bool upsideDown = false;
 
 	/* The minimum X position of this enemy object; set in the Start() method
 	 */
@@ -54,11 +52,8 @@ public class EnemyPatrol : MonoBehaviour {
 			maximumX = this.transform.position.x;
 		}
 
-		Debug.Log("Z rotation = " + this.transform.eulerAngles.z);
-
 		if (this.transform.localScale.y < 0) {
 
-			upsideDown = true;
 			Vector3 theScale = transform.localScale;
 			theScale.x *= -1;
 			transform.localScale = theScale;
