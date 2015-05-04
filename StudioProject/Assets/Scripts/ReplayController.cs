4602fd3ca3c091a84a995b0318c0b52674e210de
Brandon Gilliam <compscialien@gmail.com>
2015-05-04 01:41:21 -0400

    Debugged and worked on ReplayController, finishing it

--- StudioProject/Assets/Scripts/ReplayController.cs
+++ StudioProject/Assets/Scripts/ReplayController.cs
@@ -16,12 +16,17 @@ public class ReplayController : MonoBehaviour
 	 */
 	void Start ()
 	{
-	
 		// Get the info for the local Replays directory
+		// Note that if the directory does not exist, an exception is thrown that
+		// causes a NullPointerException with replays during FixedUpdate()
 		var dirInfo = new DirectoryInfo ("./Replays");
 
 		// Get an array of all of the FileInfo structs in the directory
 		var files = dirInfo.GetFiles ();
+
+		// A reference to the first existing player object to be found and
+		// duplicated in the loop
+		GameObject firstPlayer = null;
 
 		// Initialize the array list with the size of the files array
 		// By initializing it with the proper size, we will suffer no slowdowns
@@ -36,8 +41,25 @@ public class ReplayController : MonoBehaviour
 			// Open a stream reader for the text file
 			replay.reader = file.OpenText ();
 
-			// TODO create a new player object for each reader
-			replay.controller = null;
+			// If firstPlayer is null, then this is the first replay we've loaded
+			if (firstPlayer == null) {
+
+				// Find the player game object in the scene
+				firstPlayer = GameObject.Find ("PlayerObject");
+
+				// Get the first game object's controller
+				replay.controller = firstPlayer.GetComponent<PlayerObjectController> ();
+			}
+
+			// Otherwise, we need to copy the firstPlayer object
+			else {
+
+				// This makes an exact copy of the firstPlayer object
+				GameObject clone = Instantiate (firstPlayer);
+
+				// This function handles getting its controller
+				replay.controller = clone.GetComponent<PlayerObjectController> ();
+			}
 
 			// Add the reader to the list of readers
 			replays.Add (replay);
@@ -47,7 +69,6 @@ public class ReplayController : MonoBehaviour
 	// FixedUpdate is called once per frame
 	void FixedUpdate ()
 	{
-
 		// Process each replay object for this frame
 		foreach (ReplayPlayer replay in replays) {
 
